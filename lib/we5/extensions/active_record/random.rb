d359eb67c6d6b9dd099e1075335679f8bb410132
Björn Wolf <bjoern@we5.de>
2010-03-01 17:21:45 +0100

    Fixed namespaces

--- lib/we5/extensions/active_record/random.rb
+++ lib/we5/extensions/active_record/random.rb
@@ -1,16 +1,18 @@
 module We5
-  module ActiveRecord
-    module Random
-      
-      def self.random(count=1)
-        ids = connection.select_values("SELECT id FROM #{table_name}");
-        random_ids = Array.new(count).map do |i|
-          ids.delete(ids.rand)
+  module Extensions
+    module ActiveRecord
+      module Random
+
+        def self.random(count=1)
+          ids = connection.select_values("SELECT id FROM #{table_name}");
+          random_ids = Array.new(count).map do |i|
+            ids.delete(ids.rand)
+          end
+
+          find(random_ids.compact)
         end
 
-        find(random_ids.compact)
       end
-
     end
   end
 end
