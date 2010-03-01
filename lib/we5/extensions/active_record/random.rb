8366b3820c2fea987cbb0556669c818b5b5a6664
Björn Wolf <bjoern@we5.de>
2010-03-01 18:10:54 +0100

    Another fix

--- lib/we5/extensions/active_record/random.rb
+++ lib/we5/extensions/active_record/random.rb
@@ -2,14 +2,21 @@ module We5
   module Extensions
     module ActiveRecord
       module Random
+        
+        class << self
+          extend ClassMethods
+        end
+        
+        
+        class ClassMethods
+          def random(count=1)
+            ids = connection.select_values("SELECT id FROM #{table_name}");
+            random_ids = Array.new(count).map do |i|
+              ids.delete(ids.rand)
+            end
 
-        def self.random(count=1)
-          ids = connection.select_values("SELECT id FROM #{table_name}");
-          random_ids = Array.new(count).map do |i|
-            ids.delete(ids.rand)
+            find(random_ids.compact)
           end
-
-          find(random_ids.compact)
         end
 
       end
